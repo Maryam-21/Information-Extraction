b8c601096d774deb1f31a9bc47bbfc97866c9ee9
Maryam-21 <58235353+Maryam-21@users.noreply.github.com>
2022-04-11 03:48:46 +0200

    initial commit

---
+++ data.py
@@ -0,0 +1,34 @@
+def getTestData(name):
+    confrenceroom = ["the user can add employees information involved in the meeting then choose the meeting must provide his and phone number and the",
+                    "the user can add employees information involved in the meeting and the organizer must provide contact information such as their name phone ",
+                    "the user can add employees information involved in the meeting type and assign organizers information to it and request meeting reservation afer",
+                    "the user can add employees information involved in the meeting and the building personnel must be notified with the equipment transport request ",
+                    "the user can add employees information involved in the meeting must provide his and phone number and address ba how can a",
+                    "the user can add employees information involved in the meeting then choose the wanted room equipments have a specific needs for the",
+                    "the user can add employees information involved in the meeting type and assign organizers information to it and request meeting reservation afer",
+                    "the user can add employees information involved in the meeting must provide his and phone number and the organizers s i think ",
+                    "the user can add employees information involved in the meeting type and assign organizers information to it and request meeting reservation afer",
+                    "the user can add employees information involved in the meeting s yes there will be normal user who are responible for making ",
+                    "the user can add employees information involved in the meeting and the installing process should not take more than days ba okay",
+                    "the user can add employees information involved in the meeting type and assign organizers information to it and request meeting reservation afer ",
+                    "the user can add employees information involved in the meeting type and assign organizers information to it and request meeting reservation afer",
+                    "the user can add employees information involved in the meeting must provide his and phone number and address ba how can a",
+                    "the user can add employees information involved in the meeting then choose the wanted room equipments and update or delete them they",
+                    "the user can add employees information involved in the meeting must provide his and phone number and the organizers s i think ",
+                    "the user can add employees information involved in the meeting must provide contact information such as its size number of seats or",
+                    "the user can add employees information involved in the meeting s yes sure and the building personnel must be notified with the",
+                    "the user can add employees information involved in the meeting to be able to browse available confrence rooms or search for one",
+                    "the user can add employees information involved in the meeting to be able to add new confrenece rooms and update or delete"]
+
+    spotify = ["user should be able to search for music by name or genre or artist",
+                "he should also be able to like a song or a playlist",
+                "user should be able to perform regarding that part what is your idea regarding that we want to offer the user a",
+                "user should have a profile",
+                "yes that as well as a streaming platform for podcasts speaker a lets talk more in details about each",
+                "user should be able to like a song or a playlist",
+                "yes the profile should contain user data such as name email address liked"]
+
+    if name == "cr":
+        return confrenceroom
+    if name == "spotify":
+        return spotify
---
+++ tesy.py
@@ -0,0 +1,164 @@
+# import regex as re
+import spacy
+import data
+from spacy.matcher import Matcher
+
+from spacy import displacy
+
+nlp = spacy.load('en_core_web_sm', disable=['ner', 'textcat'])
+
+
+def main():
+    # Meeting to be processed
+    text = data.getTestData("spotify")
+    i = 1
+    services = []
+    for t in text:
+        sents = subjectverbobjectrule(t)
+        if (len(sents) !=0):
+            for sent in sents:
+                services.append(sent)
+    noDuplicateServices = []
+    for service in services:
+        if service not in noDuplicateServices:
+            noDuplicateServices.append(service)
+
+    for service in noDuplicateServices:
+         print(i, " ", service)
+         i += 1
+
+
+def subjectverbobjectrule(text):
+    doc = nlp(text)
+    sent = []
+    #print(doc)
+    # displacy.render(doc, style='dep', jupyter=True)
+
+    for token in doc:
+        # if the token is a verb
+        index = 0;
+        if token.pos_ == 'VERB' or token.pos_ == 'AUX':
+            phrase = token.text
+            # only extract noun or pronoun subjects
+            for subtok in token.rights:
+                # save the object in the phrase
+                if (subtok.dep_ in ['dobj','attr']) and (subtok.pos_ in ['NOUN', 'PROPN']):
+                    index = subtok.i
+                    if adjectiveNounRule(text,index):
+                        phrase += adjectiveNounRule(text,index) + ' ' + subtok.text
+                    else:
+                        phrase += ' ' + subtok.text
+                    for sub_tok in token.lefts:
+                        if (sub_tok.dep_ in ['nsubj', 'nsubjpass']) and (sub_tok.pos_ in ['NOUN', 'PROPN', 'PRON']):
+                            # add subject to the phrase
+                            phrase = sub_tok.text + " " + phrase
+                #elif subtok.pos_ in ['ADP']:
+                    #phrase = rule3_mod(subtok,text)
+                #   if subtok.head.pos_=='NOUN':
+                #        phrase += subtok.head.text
+                #        phrase += ' ' + subtok.text
+                #    for sub_tok in token.lefts:
+                #        if (sub_tok.dep_ in ['nsubj', 'nsubjpass']) and (sub_tok.pos_ in ['NOUN', 'PROPN', 'PRON']):
+                            # add subject to the phrase
+                #            phrase = sub_tok.text + " " + phrase
+
+                    # save the root of the verb in phrase
+                    sent.append(phrase)
+
+    return sent
+
+
+def adjectiveNounRule(text, index):
+    doc = nlp(text)
+
+    phrase = ''
+
+    for token in doc:
+
+        if token.i == index:
+            for subtoken in token.children:
+                if (subtoken.pos_ == 'ADJ'):
+                    phrase += ' ' + subtoken.text
+            break
+
+    return phrase
+
+
+def prepositionsRule(text):
+    doc = nlp(text)
+
+    sent = []
+
+    for token in doc:
+
+        # look for prepositions
+        if token.pos_ == 'ADP':
+
+            phrase = ''
+
+            # if its head word is a noun
+            if token.head.pos_ == 'NOUN':
+
+                # append noun and preposition to phrase
+                phrase += token.head.text
+                phrase += ' ' + token.text
+
+                # check the nodes to the right of the preposition
+                for right_tok in token.rights:
+                    # append if it is a noun or proper noun
+                    if (right_tok.pos_ in ['NOUN', 'PROPN']):
+                        phrase += ' ' + right_tok.text
+
+                if len(phrase) > 2:
+                    sent.append(phrase)
+
+    return sent
+
+
+def rule0(text, index):
+    doc = nlp(text)
+
+    token = doc[index]
+
+    entity = ''
+
+    for sub_tok in token.children:
+        if (sub_tok.dep_ in ['compound', 'amod']):
+            entity += sub_tok.text + ' '
+
+    entity += token.text
+
+    return entity
+
+
+def rule3_mod(token,text):
+    phrase=""
+    sent=[]
+    if token.head.pos_ == 'NOUN':
+
+        # appended rule
+        append = rule0(text, token.head.i)
+        if len(append) != 0:
+            phrase += append
+        else:
+            phrase += token.head.text
+            phrase += ' ' + token.text
+
+        for right_tok in token.rights:
+            if (right_tok.pos_ in ['NOUN', 'PROPN']):
+
+                right_phrase = ''
+                    # appended rule
+                append = rule0(text, right_tok.i)
+                if len(append) != 0:
+                    right_phrase += ' ' + append
+                else:
+                    right_phrase += ' ' + right_tok.text
+
+                    phrase += right_phrase
+
+        if len(phrase) > 2:
+            return phrase
+
+    return phrase
+main()
